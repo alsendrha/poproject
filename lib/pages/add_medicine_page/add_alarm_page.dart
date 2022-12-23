d3760ebd3b1eadee0cfc051315f2d19be7cfb59a
alsendrha <alsendrha@naver.com>
2022-12-23 12:41:07 +0900

    큰일날뻔

--- lib/pages/add_medicine_page/add_alarm_page.dart
+++ lib/pages/add_medicine_page/add_alarm_page.dart
@@ -1,6 +1,10 @@
 import 'dart:io';
 
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:poproject/components/poproject_constants.dart';
+
+import 'components/add_page_widget.dart';
 
 class AddAlarmPage extends StatelessWidget {
   const AddAlarmPage({super.key, required this.medicineImage, required this.medicienName});
@@ -11,13 +15,116 @@ class AddAlarmPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            medicineImage == null ? Container() : Image.file(medicineImage!),
-            Text(medicienName)
-          ],
+      appBar: AppBar(),
+      body: AddPageBody(
+        children: [
+          Center(
+            child: Text(
+              '매일 복약 잊지말아요!', 
+              style: Theme.of(context).textTheme.headline4,
+            )
+          ),
+          const SizedBox(height: largeSpace,),
+          Expanded(child: ListView(
+           children: const [
+            AlarmBox(),
+            AlarmBox(),
+            AlarmBox(),
+            AlarmBox(),
+            AddAlarmButton(),
+           ], 
+          ))
+          
+        ],
+      ),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: submitButtonBoxPadding,
+          child: SizedBox(
+            height: submitButtomHeight,
+            child: ElevatedButton(
+              onPressed:() {
+                
+              },
+              // },
+              style: ElevatedButton.styleFrom(
+                textStyle: Theme.of(context).textTheme.subtitle1,
+              ), 
+              child: const Text('완료'),
+            ),
+          ),
         ),
+      )
+    );
+  }
+}
+
+class AlarmBox extends StatelessWidget {
+  const AlarmBox({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: IconButton(
+            onPressed: () {
+            
+            }, 
+            icon: const Icon(CupertinoIcons.minus_circle),
+          ),
+        ),
+        Expanded(
+          flex: 5,
+          child: TextButton(
+            style: TextButton.styleFrom(
+              textStyle: Theme.of(context).textTheme.subtitle2,
+
+            ),
+            onPressed: () {
+            
+            }, 
+            child: const Text('18:00'),
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class AddAlarmButton extends StatelessWidget {
+  const AddAlarmButton({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: TextButton.styleFrom(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 0, 
+          vertical: 6
+        ),
+        textStyle: Theme.of(context).textTheme.subtitle1,
+      ),
+        
+      onPressed: () {
+        
+      },
+      child: Row(
+        children: const [
+          Expanded(
+            flex: 1,
+            child: Icon(CupertinoIcons.plus_circle_fill)
+          ),
+          Expanded(
+            flex: 5,
+            child: Center(child: Text('복용시간 추가')),
+          )
+        ],
       ),
     );
   }
